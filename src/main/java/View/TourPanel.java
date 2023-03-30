231180a4cdc202d73e9cc28ad17471f9a585aa95
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2023-03-30 10:22:13 -0500

    Hecho boton de agregar tour

--- src/main/java/View/TourPanel.java
+++ src/main/java/View/TourPanel.java
@@ -8,7 +8,21 @@ public class TourPanel extends Panel {
     public TourPanel() {
         super("Tours", "src/main/resources/Panel/Icons/001-tourists.png");
         JLabel h1 = new JLabel("Using tabs, you are now in Tours");
-        h1.setBounds(0, 0, 800, 200);
+        h1.setBounds(250, -50, 800, 200);
         this.getPanel().add(h1);
+        this.addTourButton();
     }
+
+    private void addTourButton() {
+        JButton addButton = new JButton("Add tour");
+        ImageIcon addIcon = new ImageIcon("src/main/resources/AnimalsPanel/CRUD/add.png");
+        addButton.setIcon(addIcon);
+        addButton.setBorder(null);
+        addButton.setBackground(null);
+        addButton.setBounds(350, 400, 100,50);
+
+        this.getPanel().add(addButton);
+    }
+
+
 }
