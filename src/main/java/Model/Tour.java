3d27bcea7760a1f8e5b631501982bddb7b1ced1a
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2023-03-29 19:29:12 -0500

    Estructura Java Bean en la clase Tour

--- src/main/java/Model/Tour.java
+++ src/main/java/Model/Tour.java
@@ -1,5 +1,50 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Tour {
 
+    private float price;
+    private String name;
+    private String description;
+    private ArrayList<Animal> animalList;
+
+    public Tour(float price, String name, String description, ArrayList<Animal> animalList) {
+        this.price = price;
+        this.name = name;
+        this.description = description;
+        this.animalList = animalList;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Animal> getAnimalList() {
+        return animalList;
+    }
+
+    public void setAnimalList(ArrayList<Animal> animalList) {
+        this.animalList = animalList;
+    }
 }
