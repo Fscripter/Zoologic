fa2456ea12488aeafcdb77bccc102c6a52946de4
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2023-03-31 21:43:44 -0500

    Created the add labels and textfield methods

--- src/main/java/View/AddTourWindow.java
+++ src/main/java/View/AddTourWindow.java
@@ -4,6 +4,8 @@ import Model.Panel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AddTourWindow {
 
@@ -25,9 +27,25 @@ public class AddTourWindow {
         this.addButton(panel);
         panel.setBackground(Color.DARK_GRAY);
 
+        this.addLabel("First label", 20, 20, 100, 30, panel);
+
+        this.addTextField(20, 50, 200, 30, panel);
+
         window.setSize(700, 500);
         window.setLocationRelativeTo(null);
         window.setTitle("Tour Creator");
+    }
+
+    private void createButtons() {
+
+    }
+
+    private void createLabels() {
+
+    }
+
+    private void createTextFields() {
+
     }
 
     private void addButton(JPanel panel) {
@@ -41,7 +59,31 @@ public class AddTourWindow {
         button.setBorderPainted(false);
         panel.add(button);
         button.setBounds(300, 400, 140, 40);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
+
+    private void addLabel(String text, int x, int y, int w, int h, JPanel panel) {
+        JLabel label = new JLabel(text);
+        label.setName(text);
+        label.setBounds(x, y, w, h);
+        label.setForeground(Color.white);
+
+        panel.add(label);
+    }
+
+
+    private void addTextField(int x, int y, int w, int h, JPanel panel) {
+        JTextField textField = new JTextField();
+        textField.setBounds(x, y, w, h);
+        panel.add(textField);
+    }
+
 
     public void addElement(JPanel component){
         window.add(component);
