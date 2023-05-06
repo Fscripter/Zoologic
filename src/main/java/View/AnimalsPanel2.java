66eea5ba8ca8c53aa73ac02e2cb05f675acd2d8a
Emanuel Munera <122242669+munera72@users.noreply.github.com>
2023-05-06 09:01:16 -0500

    Finished Animal Panel GUI, ready to add functionality

--- src/main/java/View/AnimalsPanel2.java
+++ src/main/java/View/AnimalsPanel2.java
@@ -28,19 +28,17 @@ public class AnimalsPanel2 extends Panel {
 
     public AnimalsPanel2(){
         super("Animals","src/main/resources/Panel/Icons/003-deer.png");
-        /*JLabel h1 = new JLabel("Using tabs, you are now in Animals");
-        h1.setBounds(0,0,800,500);
-        this.getPanel().add(h1);*/
 
         addLabel(panel);
         addTextField(panel);
-        addRadioButtons(panel);
-
+        addAcquisitionRadioButtons(panel);
+        addAnimalDivisionRadioButtons(panel);
+        addButton(panel);
     }
 
 
     // Methods for the creation of several components to add later in the panel
-    private JButton createButtons(String buttonText, int x, int y, int width, int height, String iconFilename) {
+    private JButton createButton(String buttonText, int x, int y, int width, int height, String iconFilename) {
         JButton button = new JButton(buttonText);
         button.setBorder(null);
         button.setFocusPainted(false);
@@ -61,6 +59,17 @@ public class AnimalsPanel2 extends Panel {
 
         return label;
     }
+    private JLabel createLabels(String text, int x, int y, int w, int h, float fontSize) {
+        JLabel label = new JLabel(text);
+        label.setName(text);
+        label.setBounds(x, y, w, h);
+        label.setForeground(Color.white);
+        Font font = label.getFont();
+        float size = font.getSize() + fontSize;
+        label.setFont(font.deriveFont(size));
+
+        return label;
+    }
     private JTextField createTextFields(int x, int y, int w, int h) {
         JTextField textField = new JTextField();
         textField.setBounds(x, y, w, h);
@@ -71,12 +80,22 @@ public class AnimalsPanel2 extends Panel {
         JRadioButton button = new JRadioButton(text);
         button.setBounds(x, y, w, h);
         button.setForeground(Color.white);
+        button.setBackground(Color.darkGray);
 
         return button;
+    }
+    private JPanel createInnerPanels(int x, int y, int w, int h, Color color) {
+        JPanel innerPanel = new JPanel(null);
+        innerPanel.setBounds(x, y, w, h);
+        innerPanel.setBackground(color);
+
+        return innerPanel;
     }
 
     // Methods to add the components to the panel
     private void addLabel(JPanel panel) {
+        JLabel header1 = createLabels("Add Animal",320, -70, 800, 200, 11f);
+        panel.add(header1);
 
         JLabel label1 = createLabels("Enter animal name", 20, 20, 200, 30);
         panel.add(label1);
@@ -104,17 +123,57 @@ public class AnimalsPanel2 extends Panel {
         animalSpecies = createTextFields(20, 350, 200, 30);
         panel.add(animalSpecies);
     }
-    private void addRadioButtons(JPanel panel) {
-        JPanel innerPanel = new JPanel(null);
-        innerPanel.setBounds(300, 50, 200, 300);
-        innerPanel.setBackground(Color.darkGray);
+    private void addAcquisitionRadioButtons(JPanel panel) {
+        JPanel innerPanel = createInnerPanels(300, 80, 200, 200, Color.darkGray);
 
-        JLabel label = createLabels("Acquisition Method", 30, 20, 150, 30);
+        JLabel label = createLabels("Acquisition Method", 20, 20, 150, 30, 4f);
         innerPanel.add(label);
 
-        panel.add(innerPanel);
+        ButtonGroup acquisitionButtons = new ButtonGroup();
 
+        JRadioButton button1 = createRadioButtons("Bought", 20, 60, 100, 20);
+        acquisitionButtons.add(button1);
+        innerPanel.add(button1);
+
+        JRadioButton button2 = createRadioButtons("Bornt", 20, 90, 100, 20);
+        acquisitionButtons.add(button2);
+        innerPanel.add(button2);
+
+        JRadioButton button3 = createRadioButtons("Adopted", 20, 120, 100, 20);
+        acquisitionButtons.add(button3);
+        innerPanel.add(button3);
+
+
+        panel.add(innerPanel);
+    }
+    private void addAnimalDivisionRadioButtons(JPanel panel) {
+        JPanel innerPanel = createInnerPanels(525, 80, 200, 200, Color.darkGray);
+
+        JLabel label = createLabels("Animal Division", 20, 20, 150, 30, 4f);
+        innerPanel.add(label);
+
+        ButtonGroup acquisitionButtons = new ButtonGroup();
+
+        JRadioButton button1 = createRadioButtons("Wild Animal", 20, 60, 100, 20);
+        acquisitionButtons.add(button1);
+        innerPanel.add(button1);
+
+        JRadioButton button2 = createRadioButtons("Domestic Animal", 20, 90, 170, 20);
+        acquisitionButtons.add(button2);
+        innerPanel.add(button2);
+
+        JRadioButton button3 = createRadioButtons("For adoption", 20, 120, 100, 20);
+        acquisitionButtons.add(button3);
+        innerPanel.add(button3);
+
+
+        panel.add(innerPanel);
     }
 
+    private void addButton(JPanel panel) {
+        JButton button1 = createButton("Add Animal", 300, 400, 140, 40, "src/main/resources/AnimalsPanel/CRUD/add.png");
+
+        panel.add(button1);
+    }
 
 }
