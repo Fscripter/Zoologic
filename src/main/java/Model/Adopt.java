1cae6ca18c19511cb53de4134b1e58b94b56f186
Sebastian Pelaez <sebastianpz122@gmail.com>
2023-05-13 15:16:25 -0500

    Adoption files

---
+++ src/main/java/Model/Adopt.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class Adopt {
+    private String name;
+
+    private int adoptionId;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAdoptionId() {
+        return adoptionId;
+    }
+
+    public void setAdoptionId(int adoptionId) {
+        this.adoptionId = adoptionId;
+    }
+
+    
+}
---
+++ src/main/java/View/AdoptionPanel.java
@@ -0,0 +1,9 @@
+package View;
+
+import Model.Panel;
+
+public class AdoptionPanel extends Panel{
+    public AdoptionPanel(){
+        super("Adoption","src/main/resources/Panel/Icons/006-adoption.png");
+    }
+}
