fa12c81f8ddad0d0e66d9e131a49f192ecbd279c
Sebastian Pelaez <sebastianpz122@gmail.com>
2023-05-13 20:06:16 -0500

    Adoption panel created

--- src/main/java/Model/Adopt.java
+++ src/main/java/Model/Adopt.java
@@ -1,18 +1,8 @@
 package Model;
 
 public class Adopt {
-    private String name;
-
+    
     private int adoptionId;
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getAdoptionId() {
         return adoptionId;
